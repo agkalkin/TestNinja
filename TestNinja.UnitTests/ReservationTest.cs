b8baa7376fefcd9257509afc8bd4cb3c288f99ab
Alex <agkalkin@users.noreply.github.com>
2020-07-14 02:23:09 +0200

    Section 1.8
    Pre-refactoring state.

---
+++ TestNinja.UnitTests/ReservationTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            //Arrange
+            var reservation = new Reservation();
+
+            //Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            //Assert
+
+            Assert.IsTrue(result);
+        }
+
+
+        [TestMethod]
+        public void CanBeCancelledBy_SameUserCancellingReservation_ReturnsTrue()
+        {
+            //Arrange
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user };
+            
+            //Act
+            var result = reservation.CanBeCancelledBy(user);
+
+            //Assert
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_AnotherUserCancellingReservation_ReturnsFalse()
+        {
+            //Arrange
+            var user = new User();
+            var otherUser = new User();
+            var reservation = new Reservation { MadeBy = user };
+
+            //Act
+            var result = reservation.CanBeCancelledBy(otherUser);
+
+            //Assert
+
+            Assert.IsFalse(result);
+        }
+    }
+}
